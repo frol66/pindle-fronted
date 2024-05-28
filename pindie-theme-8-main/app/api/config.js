55b9d28d80de05cb04a83e64e9aff2c1e32b151b
frol66 <vini16ipad@gmail.com>
2024-05-29 00:28:53 +0300

    Update config.js

--- pindie-theme-8-main/app/api/config.js
+++ pindie-theme-8-main/app/api/config.js
@@ -1,8 +1,9 @@
 //chto za zhest
-//export const BASE_URL = 'https://pindlefrontedwow.nomoredomainswork.ru/api'
 export const BASE_URL = 'https://pindlebackendwow.nomoredomainswork.ru/api'
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
   me: `${BASE_URL}/me`,
 }
+
+
